ce9bd343b28d501c3b9a3f2638f6537f2e6fba4e
Mahardhika Pratama <putrad578@gmail.com>
2023-09-03 22:00:38 +0700

    integrated api edit profile

--- src/redux/actions/user/editProfile.js
+++ src/redux/actions/user/editProfile.js
@@ -1,17 +1,17 @@
-import { instance } from "../../../utils/serviceApi";
+import {instance} from '../../../utils/serviceApi';
 
-const url = "https://fluffy-ox-lapel.cyclic.app";
+const url = 'https://fluffy-ox-lapel.cyclic.app';
 
-export const editMenu = (data, navigate) => async (dispatch) => {
+export const editProfile = (data, navigate) => async (dispatch) => {
   try {
-    dispatch({ type: "EDIT_PROFILE_PENDING" });
-    const response = await instance.put(`${url}/user`, data); 
-    dispatch({ type: "EDIT_PROFILE_SUCCESS", payload: response.data.data });
+    dispatch({type: 'EDIT_PROFILE_PENDING'});
+    const response = await instance.put(`${url}/user`, data);
+    dispatch({type: 'EDIT_PROFILE_SUCCESS', payload: response.data.data});
     setTimeout(() => {
-        navigate('IndexRoute');
+        navigate('Login');
       }, 1000);
   } catch (err) {
-    console.error("Error during edit profile user:", err);
-    dispatch({ type: "EDIT_PROFILE_FAILED", payload: err.response.data });
+    console.error('Error during edit profile user:', err);
+    dispatch({type: 'EDIT_PROFILE_FAILED', payload: err.response.data});
   }
 };
--- src/screens/EditProfile.js
+++ src/screens/EditProfile.js
@@ -1,53 +1,230 @@
-import {View, TouchableOpacity, Image, Text, TextInput} from 'react-native';
-import React from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  Text,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import {useDispatch, useSelector} from 'react-redux';
 import GlobalStyle from '../assets/styles/style';
+import {useNavigation} from '@react-navigation/native';
+import {editProfile} from '../redux/actions/user/editProfile';
 
 const EditProfile = () => {
+  const navigation = useNavigation();
+  const [selectedImage, setSelectedImage] = useState(null);
+  const dispatch = useDispatch();
+  const {data} = useSelector(state => state.loginUser);
+  const [inputData, setInputData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    photo_url: '',
+  });
+
+  const cameraLaunch = async () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    await launchCamera(options, res => {
+      if (res.assets && res.assets.length > 0) {
+        setSelectedImage(res.assets[0]);
+      }
+    });
+  };
+
+  const galleryLaunch = async () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    await launchImageLibrary(options, res => {
+      if (res.assets && res.assets.length > 0) {
+        setSelectedImage(res.assets[0]);
+      }
+    });
+  };
+
+  const updateProfile = async () => {
+    let formData = new FormData();
+    formData.append('username', inputData?.username);
+    formData.append('email', inputData?.email);
+    formData.append('password', inputData?.password || '');
+    if (selectedImage) {
+      formData.append('photo', {
+        uri: selectedImage?.uri,
+        name: 'photo.jpg',
+        type: 'image/jpeg',
+      });
+    }
+    dispatch(editProfile(formData, navigation.navigate));
+  };
+
+  const onChangeInput = (name, value) => {
+    setInputData({...inputData, [name]: value});
+  };
+
+  useEffect(() => {
+    if (data) {
+      setInputData({
+        username: data?.username,
+        email: data?.email,
+        photo_url: data?.photo,
+      });
+    }
+  }, [data]);
+
   return (
     <View style={GlobalStyle.container_bootstrap}>
-      <View style={{marginTop: 30}}>
-        <Text
-          style={{
-            fontSize: 25,
-            fontFamily: 'Poppins-Bold',
-            textAlign: 'center',
-            color: GlobalStyle.color_recipe.font_y,
-          }}>
-          Edit Profile
-        </Text>
-        <View style={{justifyContent: 'center', alignItems: 'center'}}>
-          <Image
-            style={{width: 120, height: 120}}
-            source={{uri: 'https://i.ibb.co/M2JSRmW/noimage.png'}}
-          />
-          <Text style={{fontFamily:'Poppins-Medium'}}>Change Photo Profile</Text>
-        </View>
-        <View style={{marginTop:10}}>
-          <Text style={{fontFamily:'Poppins-Medium'}}>Name</Text>
-          <TextInput
-            style={{backgroundColor: '#FFFFFF', borderRadius: 10, padding: 10}}
-          />
-        </View>
-        <View style={{marginTop:10}}>
-          <Text style={{fontFamily:'Poppins-Medium'}}>Email</Text>
-          <TextInput
-            style={{backgroundColor: '#FFFFFF', borderRadius: 10, padding: 10}}
-          />
-        </View>
-        <View style={{marginTop:10}}>
-          <Text style={{fontFamily:'Poppins-Medium'}}>Password</Text>
-          <TextInput
-            style={{backgroundColor: '#FFFFFF', borderRadius: 10, padding: 10}}
-          />
-        </View>
-        <View style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop:50}}>
-            <TouchableOpacity style={{backgroundColor:'#30C0F3', width:100, borderRadius:10}}>
-                <Text style={{padding:10, textAlign:'center', color:'white', fontFamily:'Poppins-Bold'}}>Save</Text>
+      <ScrollView>
+        <View style={{marginTop: 30}}>
+          <Text
+            style={{
+              fontSize: 25,
+              fontFamily: 'Poppins-Bold',
+              textAlign: 'center',
+              color: GlobalStyle.color_recipe.font_y,
+            }}>
+            Edit Profile
+          </Text>
+          <View
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 20,
+            }}>
+            {selectedImage && (
+              <Image
+                resizeMode="cover"
+                style={{
+                  height: 120,
+                  width: 120,
+                  borderRadius: 75,
+                  borderColor: GlobalStyle.color_recipe.font_y,
+                  borderWidth: 1,
+                }}
+                source={{uri: selectedImage?.uri}}
+              />
+            )}
+            {!selectedImage &&
+              inputData?.photo_url && ( // Use inputData.photo_url here
+                <Image
+                  resizeMode="cover"
+                  style={{
+                    height: 120,
+                    width: 120,
+                    borderRadius: 75,
+                    borderColor: GlobalStyle.color_recipe.font_y,
+                    borderWidth: 1,
+                  }}
+                  source={{uri: inputData?.photo_url}}
+                />
+              )}
+          </View>
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+            }}>
+            <TouchableOpacity
+              onPress={() => cameraLaunch()}
+              style={{
+                backgroundColor: '#30C0F3',
+                padding: 10,
+                width: 100,
+                borderRadius: 10,
+              }}>
+              <Text style={{textAlign: 'center', fontFamily: 'Poppins-Medium'}}>
+                Take Foto
+              </Text>
             </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => galleryLaunch()}
+              style={{
+                backgroundColor: '#F57E71',
+                padding: 10,
+                width: 100,
+                borderRadius: 10,
+              }}>
+              <Text style={{textAlign: 'center', fontFamily: 'Poppins-Medium'}}>
+                Gallery Foto
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text style={{fontFamily: 'Poppins-Medium'}}>Name</Text>
+            <TextInput
+              onChangeText={value => onChangeInput('username', value)}
+              value={inputData?.username}
+              style={{
+                backgroundColor: '#FFFFFF',
+                borderRadius: 10,
+                padding: 10,
+              }}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text style={{fontFamily: 'Poppins-Medium'}}>Email</Text>
+            <TextInput
+              onChangeText={value => onChangeInput('email', value)}
+              value={inputData?.email}
+              style={{
+                backgroundColor: '#FFFFFF',
+                borderRadius: 10,
+                padding: 10,
+              }}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text style={{fontFamily: 'Poppins-Medium'}}>Password</Text>
+            <TextInput
+              onChangeText={value => onChangeInput('password', value)}
+              value={inputData?.password}
+              style={{
+                backgroundColor: '#FFFFFF',
+                borderRadius: 10,
+                padding: 10,
+              }}
+              secureTextEntry={true}
+            />
+          </View>
+          <View
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 50,
+            }}>
+            <TouchableOpacity
+              onPress={updateProfile}
+              style={{
+                backgroundColor: '#30C0F3',
+                width: 100,
+                borderRadius: 10,
+              }}>
+              <Text
+                style={{
+                  padding: 10,
+                  textAlign: 'center',
+                  color: 'white',
+                  fontFamily: 'Poppins-Bold',
+                }}>
+                Save
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </View>
   );
 };
