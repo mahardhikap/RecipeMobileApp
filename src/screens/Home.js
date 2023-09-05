c32b4ed17df2c6b325ef9819ec1cb3ab6349ea22
Mahardhika Pratama <putrad578@gmail.com>
2023-09-05 18:54:57 +0700

    fix bug at home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -29,14 +29,14 @@ const Home = () => {
   }, [search]);
   const navigation = useNavigation();
   return (
-    <ScrollView>
+    <View>
+      <View style={GlobalStyle.container_bootstrap}>
+        <View style={{marginTop: 20}}>
+          <SearchBar changeText={handleSearchChange} />
+        </View>
+      </View>
       {search ? (
-        <View>
-          <View style={GlobalStyle.container_bootstrap}>
-            <View style={{marginTop: 20}}>
-              <SearchBar changeText={handleSearchChange} />
-            </View>
-          </View>
+        <ScrollView>
           <View style={GlobalStyle.container_bootstrap}>
             {data?.rows?.map(item => {
               return (
@@ -102,16 +102,57 @@ const Home = () => {
               );
             })}
           </View>
-        </View>
+        </ScrollView>
       ) : (
-        <View style={GlobalStyle.container_bootstrap}>
-          <View style={{marginTop: 20}}>
-            <SearchBar changeText={handleSearchChange} />
-          </View>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('PopularMenu');
-            }}>
+        <ScrollView>
+          <View style={GlobalStyle.container_bootstrap}>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('PopularMenu');
+              }}>
+              <Text
+                style={{
+                  fontFamily: 'Poppins-SemiBold',
+                  fontSize: 20,
+                  marginTop: 40,
+                  padding: 0,
+                }}>
+                Popular Recipes
+              </Text>
+            </TouchableOpacity>
+            <Text style={{fontFamily: 'Poppins-Bold'}}>Popular check</Text>
+            <ScrollView horizontal={true} style={{flexDirection: 'row'}}>
+              <View style={{position: 'relative', marginRight: 10}}>
+                <Image source={require('../assets/images/Rectangle_7.png')} />
+                <Text
+                  style={{
+                    color: 'white',
+                    position: 'absolute',
+                    padding: 15,
+                    fontFamily: 'Poppins-Bold',
+                    fontSize: 18,
+                    width: '50%',
+                    bottom: -5,
+                  }}>
+                  Sandwich With Egg
+                </Text>
+              </View>
+              <View style={{position: 'relative'}}>
+                <Image source={require('../assets/images/Rectangle_7.png')} />
+                <Text
+                  style={{
+                    color: 'white',
+                    position: 'absolute',
+                    padding: 15,
+                    fontFamily: 'Poppins-Bold',
+                    fontSize: 18,
+                    width: '50%',
+                    bottom: -5,
+                  }}>
+                  Sandwich With Egg
+                </Text>
+              </View>
+            </ScrollView>
             <Text
               style={{
                 fontFamily: 'Poppins-SemiBold',
@@ -119,160 +160,123 @@ const Home = () => {
                 marginTop: 40,
                 padding: 0,
               }}>
-              Popular Recipes
+              New Recipes
             </Text>
-          </TouchableOpacity>
-          <Text style={{fontFamily: 'Poppins-Bold'}}>Popular check</Text>
-          <ScrollView horizontal={true} style={{flexDirection: 'row'}}>
-            <View style={{position: 'relative', marginRight: 10}}>
-              <Image source={require('../assets/images/Rectangle_7.png')} />
-              <Text
-                style={{
-                  color: 'white',
-                  position: 'absolute',
-                  padding: 15,
-                  fontFamily: 'Poppins-Bold',
-                  fontSize: 18,
-                  width: '50%',
-                  bottom: -5,
-                }}>
-                Sandwich With Egg
-              </Text>
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <View>
+                <Image source={require('../assets/images/Group_48.png')} />
+                <Text
+                  style={{textAlign: 'center', fontFamily: 'Poppins-Medium'}}>
+                  Soup
+                </Text>
+              </View>
+              <View>
+                <Image source={require('../assets/images/Group_47.png')} />
+                <Text
+                  style={{textAlign: 'center', fontFamily: 'Poppins-Medium'}}>
+                  Chicken
+                </Text>
+              </View>
+              <View>
+                <Image source={require('../assets/images/Group_49.png')} />
+                <Text
+                  style={{textAlign: 'center', fontFamily: 'Poppins-Medium'}}>
+                  Seafood
+                </Text>
+              </View>
+              <View>
+                <Image source={require('../assets/images/Group_50.png')} />
+                <Text
+                  style={{textAlign: 'center', fontFamily: 'Poppins-Medium'}}>
+                  Dessert
+                </Text>
+              </View>
             </View>
-            <View style={{position: 'relative'}}>
-              <Image source={require('../assets/images/Rectangle_7.png')} />
-              <Text
-                style={{
-                  color: 'white',
-                  position: 'absolute',
-                  padding: 15,
-                  fontFamily: 'Poppins-Bold',
-                  fontSize: 18,
-                  width: '50%',
-                  bottom: -5,
-                }}>
-                Sandwich With Egg
-              </Text>
-            </View>
-          </ScrollView>
-          <Text
-            style={{
-              fontFamily: 'Poppins-SemiBold',
-              fontSize: 20,
-              marginTop: 40,
-              padding: 0,
-            }}>
-            New Recipes
-          </Text>
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <View>
-              <Image source={require('../assets/images/Group_48.png')} />
-              <Text style={{textAlign: 'center', fontFamily: 'Poppins-Medium'}}>
-                Soup
-              </Text>
-            </View>
-            <View>
-              <Image source={require('../assets/images/Group_47.png')} />
-              <Text style={{textAlign: 'center', fontFamily: 'Poppins-Medium'}}>
-                Chicken
-              </Text>
-            </View>
-            <View>
-              <Image source={require('../assets/images/Group_49.png')} />
-              <Text style={{textAlign: 'center', fontFamily: 'Poppins-Medium'}}>
-                Seafood
-              </Text>
-            </View>
-            <View>
-              <Image source={require('../assets/images/Group_50.png')} />
-              <Text style={{textAlign: 'center', fontFamily: 'Poppins-Medium'}}>
-                Dessert
-              </Text>
-            </View>
+            <Text
+              style={{
+                fontFamily: 'Poppins-SemiBold',
+                fontSize: 20,
+                marginTop: 40,
+                padding: 0,
+              }}>
+              Popular For You
+            </Text>
+            <ScrollView horizontal={true} style={{flexDirection: 'row'}}>
+              <View style={{width: 180, position: 'relative', marginRight: 10}}>
+                <Image
+                  style={{borderRadius: 10}}
+                  source={require('../assets/images/Rectangle_55.png')}
+                />
+                <View
+                  style={{
+                    backgroundColor: 'white',
+                    position: 'absolute',
+                    width: '100%',
+                    bottom: 0,
+                    borderBottomEndRadius: 10,
+                    borderBottomLeftRadius: 10,
+                    padding: 5,
+                  }}>
+                  <Text style={{fontFamily: 'Poppins-ExtraBold'}}>
+                    Beef Steak
+                  </Text>
+                  <Text style={{fontFamily: 'Poppins-Medium'}}>
+                    Beef steak with nopales, tartare ....
+                  </Text>
+                </View>
+              </View>
+              <View style={{width: 180, position: 'relative', marginRight: 10}}>
+                <Image
+                  style={{borderRadius: 10}}
+                  source={require('../assets/images/Rectangle_55.png')}
+                />
+                <View
+                  style={{
+                    backgroundColor: 'white',
+                    position: 'absolute',
+                    width: '100%',
+                    bottom: 0,
+                    borderBottomEndRadius: 10,
+                    borderBottomLeftRadius: 10,
+                    padding: 5,
+                  }}>
+                  <Text style={{fontFamily: 'Poppins-ExtraBold'}}>
+                    Beef Steak
+                  </Text>
+                  <Text style={{fontFamily: 'Poppins-Medium'}}>
+                    Beef steak with nopales, tartare ....
+                  </Text>
+                </View>
+              </View>
+              <View style={{width: 180, position: 'relative', marginRight: 10}}>
+                <Image
+                  style={{borderRadius: 10}}
+                  source={require('../assets/images/Rectangle_55.png')}
+                />
+                <View
+                  style={{
+                    backgroundColor: 'white',
+                    position: 'absolute',
+                    width: '100%',
+                    bottom: 0,
+                    borderBottomEndRadius: 10,
+                    borderBottomLeftRadius: 10,
+                    padding: 5,
+                  }}>
+                  <Text style={{fontFamily: 'Poppins-ExtraBold'}}>
+                    Beef Steak
+                  </Text>
+                  <Text style={{fontFamily: 'Poppins-Medium'}}>
+                    Beef steak with nopales, tartare ....
+                  </Text>
+                </View>
+              </View>
+            </ScrollView>
           </View>
-          <Text
-            style={{
-              fontFamily: 'Poppins-SemiBold',
-              fontSize: 20,
-              marginTop: 40,
-              padding: 0,
-            }}>
-            Popular For You
-          </Text>
-          <ScrollView horizontal={true} style={{flexDirection: 'row'}}>
-            <View style={{width: 180, position: 'relative', marginRight: 10}}>
-              <Image
-                style={{borderRadius: 10}}
-                source={require('../assets/images/Rectangle_55.png')}
-              />
-              <View
-                style={{
-                  backgroundColor: 'white',
-                  position: 'absolute',
-                  width: '100%',
-                  bottom: 0,
-                  borderBottomEndRadius: 10,
-                  borderBottomLeftRadius: 10,
-                  padding: 5,
-                }}>
-                <Text style={{fontFamily: 'Poppins-ExtraBold'}}>
-                  Beef Steak
-                </Text>
-                <Text style={{fontFamily: 'Poppins-Medium'}}>
-                  Beef steak with nopales, tartare ....
-                </Text>
-              </View>
-            </View>
-            <View style={{width: 180, position: 'relative', marginRight: 10}}>
-              <Image
-                style={{borderRadius: 10}}
-                source={require('../assets/images/Rectangle_55.png')}
-              />
-              <View
-                style={{
-                  backgroundColor: 'white',
-                  position: 'absolute',
-                  width: '100%',
-                  bottom: 0,
-                  borderBottomEndRadius: 10,
-                  borderBottomLeftRadius: 10,
-                  padding: 5,
-                }}>
-                <Text style={{fontFamily: 'Poppins-ExtraBold'}}>
-                  Beef Steak
-                </Text>
-                <Text style={{fontFamily: 'Poppins-Medium'}}>
-                  Beef steak with nopales, tartare ....
-                </Text>
-              </View>
-            </View>
-            <View style={{width: 180, position: 'relative', marginRight: 10}}>
-              <Image
-                style={{borderRadius: 10}}
-                source={require('../assets/images/Rectangle_55.png')}
-              />
-              <View
-                style={{
-                  backgroundColor: 'white',
-                  position: 'absolute',
-                  width: '100%',
-                  bottom: 0,
-                  borderBottomEndRadius: 10,
-                  borderBottomLeftRadius: 10,
-                  padding: 5,
-                }}>
-                <Text style={{fontFamily: 'Poppins-ExtraBold'}}>
-                  Beef Steak
-                </Text>
-                <Text style={{fontFamily: 'Poppins-Medium'}}>
-                  Beef steak with nopales, tartare ....
-                </Text>
-              </View>
-            </View>
-          </ScrollView>
-        </View>
+        </ScrollView>
       )}
-    </ScrollView>
+    </View>
   );
 };
 
