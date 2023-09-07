cc0bad6b926558bbf6d7223f0f3c7bd8f54147ee
Mahardhika Pratama <putrad578@gmail.com>
2023-09-07 10:06:53 +0700

    add one signal/push notification

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ import storages from './src/redux/store';
 import React from 'react';
 import IndexRoute from './src/IndexRoute';
 import AppNavigator from './src/NavContainer';
+import {ONESIGNAL_APP_ID} from "@env"
+
+//import one signal
+import { LogLevel, OneSignal } from 'react-native-onesignal';
+// OneSignal Initialization
+OneSignal.initialize(ONESIGNAL_APP_ID);
+OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+// requestPermission will show the native iOS or Android notification permission prompt.
+// We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+OneSignal.Notifications.requestPermission(true);
+
+// Method for listening for notification clicks
+OneSignal.Notifications.addEventListener('click', (event) => {
+  console.log('OneSignal: notification clicked:', event);
+});
 
 const {store, persistor} = storages();
 
